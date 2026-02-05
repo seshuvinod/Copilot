f9920703debb4d2d4c85169ad69bccd691d9fee0
vinod <nsvinod@hotmail.com>
2026-02-05 11:45:22 -0500

    k2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,7 +21,7 @@ namespace Copilot.Controllers
         [Route("/")]
         public IActionResult Index()
         {
-            ViewBag.Name = "VINOD NS";
+            ViewBag.Name = "VINOD NS 45";
                 return View();
         }
 
