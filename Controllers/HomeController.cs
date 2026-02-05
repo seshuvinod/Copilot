5ad343e38bee7409d827333c8a18ab49ddd20a61
vinod <nsvinod@hotmail.com>
2026-02-05 13:03:34 -0500

    cmt3

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,7 +21,7 @@ namespace Copilot.Controllers
         [Route("/")]
         public IActionResult Index()
         {
-            ViewBag.Name = "VINOD F1";
+            ViewBag.Name = "VINOD F2";
                 return View();
         }
 
